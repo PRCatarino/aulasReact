def41e67f4ecf8227cad567cd2b00d70df8ea0c4
PRCatarino <paulo061104@gmail.com>
2023-02-01 21:36:54 -0300

    aula10

---
+++ aula11/src/reducer.jsx
@@ -0,0 +1,16 @@
+export const initialStateProdutos = ['produto1', 'produto2'];
+
+
+export function ProductReducer(state, action) {
+    
+    switch (action.type) {
+      case "PRODUCT_ADD":
+        return [...state, action.payload];
+      case "REMOVE_PRODUCT":
+        return state.slice(0, state.length -1);
+      default:
+        return state;
+    }
+  }
+
+   
